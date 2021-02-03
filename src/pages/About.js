a00b5a8415f043b92bd45487feb69e46b9ba48b4
David Wu <david204code@gmail.com>
2021-02-03 13:24:21 +0000

    set up the About and Error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className ="section about-section">
+      <h1 className ="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur tempora culpa harum! Quae sequi magnam nostrum numquam a atque voluptatum unde libero illum deserunt id aliquam blanditiis dolore sapiente, tenetur cupiditate est. Omnis tempore similique perferendis incidunt molestiae deleniti repellat.
+      </p>
+    </section>
   )
-}
+};
 
-export default About
+export default About;
