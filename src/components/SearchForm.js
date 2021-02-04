b9f8561d8fe541252d6982eb1c335111b81b7d06
David Wu <david204code@gmail.com>
2021-02-04 14:44:24 +0000

    Add the focus function with React.useEffect() on the search box and handleSubmit to preventDeafult onSubmit

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -8,13 +8,22 @@ const SearchForm = () => {
   // using useRef()
   const searchValue = React.useRef('');
 
+  // focus function
+  React.useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className ="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit ={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">
             Search your favorite cocktail
