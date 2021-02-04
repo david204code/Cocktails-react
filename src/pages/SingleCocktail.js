0106d6e833301e6018d64efe24c946884d282fe2
David Wu <david204code@gmail.com>
2021-02-04 16:26:49 +0000

    Getting the SingleCocktail data using async function with React.useEffect()

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import Loading from '../components/Loading'
-import { useParams, Link } from 'react-router-dom'
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import React from "react";
+import Loading from "../components/Loading";
+import { useParams, Link } from "react-router-dom";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
+  
+  const { id } = useParams();
+  const [ loading, setLoading ] = React.useState(false);
+  const [ cocktail, setCocktail ] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail () {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        console.log(data);
+      }
+      catch {
+
+      }
+    }
+    getCocktail();
+  }, [id])
+
   return (
     <div>
-      <h2>single cocktail page </h2>
+      {id}
     </div>
-  )
-}
+  );
+};
 
-export default SingleCocktail
+export default SingleCocktail;
